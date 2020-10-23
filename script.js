0266883667b1e824d0dcfbf07ce282bad488ae07
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-10-24 02:26:48 +0900

    宿題2問めまで

---
+++ script.js
@@ -0,0 +1,20 @@
+// このファイルに解答を書いてください。
+// 問題文はindex.htmlにあります。
+
+
+// Q 1
+let hello = document.getElementsByTagName('p');
+console.log(hello[2]);
+
+// Q 2
+let colorq2 = document.getElementById('q2-text');
+colorq2.style.color = 'blue';
+// Q 3
+
+
+// Q 4 (難)
+// 「for of」を使います。
+
+// Q 5
+
+
