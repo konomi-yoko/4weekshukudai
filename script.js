8594dda0cc5b8efdbf7c5a43c35d51dfcc73bef2
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-10-25 18:22:01 +0900

    宿題完成

--- script.js
+++ script.js
@@ -7,14 +7,23 @@ let hello = document.getElementsByTagName('p');
 console.log(hello[2]);
 
 // Q 2
-let colorq2 = document.getElementById('q2-text');
-colorq2.style.color = 'blue';
-// Q 3
+let msg = document.getElementById('q2-text');
+msg.textContent = '立つ鳥跡を濁さず';
 
+// Q 3
+let q3 = document.getElementById('q3-text');
+q3.style.color = 'red';
 
 // Q 4 (難)
 // 「for of」を使います。
+let q4 = document.querySelectorAll('.q4-text');
+for (colo of q4) {
+	colo.style.color = 'green';
+}
 
 // Q 5
 
-
+let q5 = document.querySelectorAll('.q5-text');
+for (colo of q5) {
+	colo.style.backgroundColor = 'pink';
+}
