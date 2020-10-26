86d2700a266aaa25fc0e8355a2e345919fbf2c4e
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-10-26 17:07:56 +0900

    宿題直し

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // このファイルに解答を書いてください。
 // 問題文はindex.htmlにあります。
 
+// function answer(){
+// 	var q1_text = document.getElementById('q1-text');
+// console.log(q1_text.textContent);
+
+// }
 
 // Q 1
 let hello = document.getElementsByTagName('p');
 console.log(hello[2]);
+
+let messageSpan = document.getElementById('q1-text');
+console.log(messageSpan.textContent);
 
 // Q 2
 let msg = document.getElementById('q2-text');
@@ -17,9 +25,17 @@ q3.style.color = 'red';
 // Q 4 (難)
 // 「for of」を使います。
 let q4 = document.querySelectorAll('.q4-text');
+console.log(q4);
 for (colo of q4) {
 	colo.style.color = 'green';
 }
+
+// var messageSpan = document.getElementsByClassName('q4-text');
+// console.log(messageSpan);
+// for (var i = 0;i < messageSpan.length;i++)
+// {
+// 	messageSpan[i].style.color = 'green';
+// }
 
 // Q 5
 
@@ -27,3 +43,7 @@ let q5 = document.querySelectorAll('.q5-text');
 for (colo of q5) {
 	colo.style.backgroundColor = 'pink';
 }
+
+
+
+
